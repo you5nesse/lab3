a310c16a8af6a8eab2d21e2beba4005677d11709
you5nesse <youn17esse@gmail.com>
2025-11-25 14:27:54 +0100

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,11 @@
+#operation
+a = 15
+b = 4
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} // {b} = {a // b}")    
+print(f"{a} % {b} = {a % b}")      
+print(f"{a} ** {b} = {a ** b}")
+ 
